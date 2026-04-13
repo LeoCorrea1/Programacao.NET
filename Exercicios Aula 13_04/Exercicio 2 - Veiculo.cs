d0cf89a54d2048b977270674e908af04604537bd
Leonardo Vasconcelos Correa <leonardo.vasconcelos@ufn.edu.br>
2026-04-13 16:34:07 -0300

    Update Exercicio 2 - Veiculo.cs

--- Exercicios Aula 13_04/Exercicio 2 - Veiculo.cs
+++ Exercicios Aula 13_04/Exercicio 2 - Veiculo.cs
@@ -6,13 +6,17 @@ namespace ProjetoAula13_04
     abstract class Veiculo
     {
         public string Marca, Modelo;
+        public int consumo;
 
         public abstract string Dirigir();
 
-        public Veiculo(string Marca, string Modelo)
+        public abstract string CalcularConsumo();
+
+        public Veiculo(string Marca, string Modelo , int Consumo)
         {
             this.Marca = Marca;
             this.Modelo = Modelo;
+            this.consumo = Consumo;
         }
     }
 
@@ -20,7 +24,7 @@ namespace ProjetoAula13_04
     {
         public int portas;
 
-        public Carro(string Marca, string Modelo, int portas) : base(Marca, Modelo)
+        public Carro(string Marca, string Modelo, int portas , int consumo) : base(Marca, Modelo,consumo)
         {
             this.portas = portas;
         }
@@ -29,13 +33,21 @@ namespace ProjetoAula13_04
         {
             return "DIRIGINDO O CARRO MARCA : " + Marca + " Modelo : " + Modelo + " Portas : " + portas;
         }
+
+        public override string CalcularConsumo()
+        {
+            return "Consumo Carro :" + consumo * 10;
+
+        }
+
+
     }
 
     class Moto : Veiculo
     {
         public int cilindrada;
 
-        public Moto(string Marca, string Modelo, int cilindrada) : base(Marca, Modelo)
+        public Moto(string Marca, string Modelo, int cilindrada , int consumo) : base(Marca, Modelo , consumo)
         {
             this.cilindrada = cilindrada;
         }
@@ -43,6 +55,12 @@ namespace ProjetoAula13_04
         public override string Dirigir()
         {
             return "DIRIGINDO A MOTO MARCA : " + Marca + " Modelo : " + Modelo + " Cilindradas : " + cilindrada;
+        }
+
+        public override string CalcularConsumo()
+        {
+            return "Consumo Moto :" + consumo * 7;
+
         }
     }
 
@@ -52,12 +70,14 @@ namespace ProjetoAula13_04
         {
             List<Veiculo> v = new List<Veiculo>();
 
-            v.Add(new Carro("fiat", "uno", 2));
-            v.Add(new Moto("honda", "f200", 1500));
+            v.Add(new Carro("fiat", "uno", 2,5));
+            v.Add(new Moto("honda", "f200", 1500,2));
 
             foreach (Veiculo a in v)
             {
                 Console.WriteLine(a.Dirigir());
+                Console.WriteLine(a.CalcularConsumo());
+                Console.Write("\n");
             }
         }
     }
